ac61e9a68c49950ebb19f7e911a98707ad61d73d
Lucas McLaren <lucas.mclaren@scout24.com>
2017-04-27 15:23:52 +0200

    PE-1484 changed log level for events with exceptions to error. BatchUpdateExceptions are now logged with all chained errors

--- app/toguru/logging/EventPublisher.scala
+++ app/toguru/logging/EventPublisher.scala
@@ -1,5 +1,7 @@
 package toguru.logging
 
+import java.sql.BatchUpdateException
+
 import net.logstash.logback.marker.LogstashMarker
 import net.logstash.logback.marker.Markers.appendEntries
 import org.slf4j.{Logger, LoggerFactory}
@@ -24,11 +26,21 @@ object EventPublisher {
 
   def event(name: String, exception: Throwable, fields: (String, Any)*): Unit = {
     val eventMarkers = markers(name, fields :+ ("exception_type" -> exception.getClass.getName))
-
-    eventLogger.info(eventMarkers, exception.getMessage, exception)
+    exception match {
+      case e: BatchUpdateException => eventLogger.error(eventMarkers, nestedExeceptionAsString(e), exception)
+      case _ => eventLogger.error(eventMarkers, exception.getMessage, exception)
+    }
   }
 
   def event(e: Event): Unit = event(e.eventName, e.eventFields: _*)
+
+  def nestedExeceptionAsString(exception: BatchUpdateException): String = {
+    val exceptions = exception.getMessage
+    while(exception.getNextException != null) {
+       exceptions + "\n\n" + exception.getNextException.getMessage
+    }
+    exceptions
+  }
 
   private def markers(name: String, fields: Seq[(String, Any)]): LogstashMarker =
     appendEntries(Map(fields: _*).updated("@name", name).asJava)
